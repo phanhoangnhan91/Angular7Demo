d7b987a3b706462523b7b85c0a69d58606ff7dd7
Nhan Phan Hoang <nhan.ph2609@sv.sgu.edu.vn>
2019-06-07 23:34:47 +0700

    [FE-2] 2 ways binding, ngFor, css

--- Angular7/ClientApp/src/app/heros/heros.component.ts
+++ Angular7/ClientApp/src/app/heros/heros.component.ts
@@ -10,6 +10,18 @@ export class HerosComponent implements OnInit {
 
     constructor() { }
     hero: Hero = new Hero();
+    heros: Hero[] = [
+        { id: 11, name: 'Dr Nice' },
+        { id: 12, name: 'Narco' },
+        { id: 13, name: 'Bombasto' },
+        { id: 14, name: 'Celeritas' },
+        { id: 15, name: 'Magneta' },
+        { id: 16, name: 'RubberMan' },
+        { id: 17, name: 'Dynama' },
+        { id: 18, name: 'Dr IQ' },
+        { id: 19, name: 'Magma' },
+        { id: 20, name: 'Tornado' }
+    ];
 
     ngOnInit() {
         this.hero.id = 1;

1a3306cb25602d51e2a5ba28213526830ced6425
Nhan Phan Hoang <nhan.ph2609@sv.sgu.edu.vn>
2019-06-07 22:49:05 +0700

    [FE-6] add component

--- Angular7/ClientApp/src/app/app.module.ts
+++ Angular7/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HerosComponent } from './heros/heros.component';
+import { MyComponentComponent } from './folder1/my-component/my-component.component';
 
 @NgModule({
   declarations: [
@@ -16,16 +18,20 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    HerosComponent,
+    MyComponentComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+        { path: '', component: HomeComponent, pathMatch: 'full' },
+        { path: 'counter', component: CounterComponent },
+        { path: 'fetch-data', component: FetchDataComponent },
+        { path: 'hero', component: HerosComponent },
+        { path: 'my-component', component: MyComponentComponent },
     ])
   ],
   providers: [],
